64ff7e575f0365ce3fcfcbb0bc2b25e3445de7bc
Julio Carvajal <soporte@creasyscr.com>
2023-07-06 10:59:05 -0600

    uso de useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import {useState} from 'react'
 import './App.css'  //se le dice que importe el css para esta App y tiene el mismo nombre
 import { TwitterFollowCard } from './TwitterFollowCard.jsx' //importe funcion TwitterFollowCard de archivo jsx del mismo nombre
 //export se utiliza para que la funcion sea utilizada desde otros modulos, en este caso desde main.jsx
 export function App(){ 
     //const formatUserName = (userName) => `@${userName}`  //funcion que recibe parametro userName y que devuelve @userName
-    //const formatUserName = (userName) => `@${userName}`
+    const [name,setName] = useState('midudev')
+    console.log('render with name', name)
     return(
         /* en vez de  <React.Fragment></React.Fragment>  se agrega <> sin embargo en este caso 
         luego de utilizar <> se utilizo la etiqueta section para ponerle la clase css .App que se encuentra
@@ -13,12 +15,15 @@ export function App(){
         se llama luego a la funcion TwitterFollowCard y se le envían los parámetros
         */
         <section className="App">  
-        <TwitterFollowCard isFollowing userName="midudev">
+        <TwitterFollowCard  userName={name} initialisFollowing={true}>
          Miguel Angel Duran
+        </TwitterFollowCard>
+        <TwitterFollowCard  userName="pheralb">
+        Pablo Hernandez
          </TwitterFollowCard>
-        <TwitterFollowCard isFollowing={false} userName="pheralb">
-         Pablo Hernandez
-         </TwitterFollowCard>
+         <button onClick={()=>setName('pedromichel')}>
+           Cambio Nombre    
+         </button>
         </section>
     )
 
--- src/TwitterFollowCard.jsx
+++ src/TwitterFollowCard.jsx
@@ -1,6 +1,19 @@
-export function TwitterFollowCard({children,userName,  isFollowing}){
+import {useState} from 'react'
+
+export function TwitterFollowCard({children,userName,initialisFollowing}){
+    const [isFollowing, setIsFollowing] = useState(initialisFollowing)
+    console.log('[TwitterFollowCard] render with username:', userName)
     //ojo que para la variable se usa apostrofe en vez de comillas
     const imageSrc=`https://unavatar.io/${userName}`
+    const text = isFollowing ? 'Siguiendo' : 'Seguir'
+    const buttonClassName = isFollowing
+      ? 'tw-followCard-button is-following'
+      : 'tw-followCard-button'
+
+    const handleClick =() => {
+        setIsFollowing(!isFollowing)
+    }
+
     //const addAt = (username) => `@${userName}`
     //console.log(isFollowing);
     return(
@@ -8,6 +21,7 @@ export function TwitterFollowCard({children,userName,  isFollowing}){
     //cada etiqueta de html tiene su identificacion de css del archivo App.css
     <article className='tw-followCard'>
         <header className='tw-followCard-header'>
+            { /*aqui va las notas*/}
             <img className='tw-followCard-avatar'
              alt="El avatar de midudev" src={imageSrc}/> 
             <div className='tw-followCard-info'>
@@ -16,8 +30,8 @@ export function TwitterFollowCard({children,userName,  isFollowing}){
             </div>
         </header>
         <aside>
-            <button className='tw-followCard-button'>
-                Seguir
+            <button className={buttonClassName} onClick={handleClick}>
+                {text}
             </button>
         </aside>
     </article>    
